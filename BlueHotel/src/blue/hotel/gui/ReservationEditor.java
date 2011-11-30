e9442618cb8a3cdc5514496d44b11163c214c058
muellers <stefan.mueller@vol.at>
2011-12-01 00:31:00 +0100

    GUI überarbeitet
    Signed-off-by: muellers <stefan.mueller@vol.at>

--- BlueHotel/src/blue/hotel/gui/ReservationEditor.java
+++ BlueHotel/src/blue/hotel/gui/ReservationEditor.java
@@ -41,6 +41,10 @@ import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.RowSpec;
 import com.toedter.calendar.JDateChooser;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.FlowLayout;
 
 @SuppressWarnings("serial")
 public class ReservationEditor extends JDialog implements Editor<Reservation>{
@@ -69,22 +73,30 @@ public class ReservationEditor extends JDialog implements Editor<Reservation>{
 	}
 
 	public ReservationEditor() {
+		setResizable(false);
 		setModalityType(ModalityType.APPLICATION_MODAL);
 		setTitle("new Reservation");
-		setSize(400, 689);
+		setSize(400, 748);
 		setLocationRelativeTo(null);
-		getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_editor = new JPanel();
-		panel_editor.setBorder(new EmptyBorder(0, 0, 0, 0));
-		getContentPane().add(panel_editor);
-		panel_editor.setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JPanel customerPanel = new JPanel();
-		customerPanel.setBorder(new TitledBorder(null, "Customer", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_editor.add(customerPanel);
 		
 		customerListModel = new DefaultListModel();
+		
+		roomReservationListModel = new DefaultListModel();
+		
+		Calendar cal = Calendar.getInstance();
+		Date now = new Date();
+		cal.setTime(now);
+		
+		cal.add(Calendar.DAY_OF_YEAR, 1);
+		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel_2 = new JPanel();
+		getContentPane().add(panel_2);
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel customerPanel = new JPanel();
+		panel_2.add(customerPanel);
+		customerPanel.setBorder(new TitledBorder(null, "Customer", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		customerPanel.setLayout(new FormLayout(new ColumnSpec[] {
 				ColumnSpec.decode("15px"),
 				ColumnSpec.decode("344px"),},
@@ -159,11 +171,12 @@ public class ReservationEditor extends JDialog implements Editor<Reservation>{
 			}
 		});
 		
-		JPanel roomPanel = new JPanel();
-		roomPanel.setBorder(new TitledBorder(null, "Room", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_editor.add(roomPanel);
+		JPanel panel_3 = new JPanel();
+		getContentPane().add(panel_3);
 		
-		roomReservationListModel = new DefaultListModel();
+		JPanel roomPanel = new JPanel();
+		panel_3.add(roomPanel);
+		roomPanel.setBorder(new TitledBorder(null, "Room", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		roomPanel.setLayout(new FormLayout(new ColumnSpec[] {
 				ColumnSpec.decode("15px"),
 				ColumnSpec.decode("344px"),},
@@ -171,38 +184,43 @@ public class ReservationEditor extends JDialog implements Editor<Reservation>{
 				FormFactory.NARROW_LINE_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.LINE_GAP_ROWSPEC,
-				RowSpec.decode("46px"),
-				RowSpec.decode("42px"),}));
+				RowSpec.decode("60px"),
+				RowSpec.decode("max(40dlu;default)"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("33px"),}));
 		
 		roomBox = new JComboBox();
 		roomPanel.add(roomBox, "2, 2, fill, default");
 		roomReservationList = new JList(roomReservationListModel);
-		roomPanel.add(roomReservationList, "2, 4, left, fill");
+		roomReservationList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		roomPanel.add(roomReservationList, "2, 4, fill, fill");
 		
 		JPanel roomReservationPanel = new JPanel();
-		roomPanel.add(roomReservationPanel, "2, 4, fill, fill");
+		roomPanel.add(roomReservationPanel, "2, 5, fill, fill");
 		roomReservationPanel.setLayout(new FormLayout(new ColumnSpec[] {
 				ColumnSpec.decode("149px"),
 				ColumnSpec.decode("left:195px"),
 				FormFactory.RELATED_GAP_COLSPEC,},
 			new RowSpec[] {
+				RowSpec.decode("max(7dlu;default)"),
+				FormFactory.RELATED_GAP_ROWSPEC,
 				RowSpec.decode("26px"),
 				RowSpec.decode("26px"),}));
 		
 		JLabel lblAduldts = new JLabel("Adult(s):");
-		roomReservationPanel.add(lblAduldts, "1, 1, left, top");
+		roomReservationPanel.add(lblAduldts, "1, 3, left, top");
 		
 		adultSpinner = new JSpinner(new SpinnerNumberModel(1, 1, 100, 1));
-		roomReservationPanel.add(adultSpinner, "2, 1, fill, top");
+		roomReservationPanel.add(adultSpinner, "2, 3, fill, top");
 		
 		JLabel lblKids = new JLabel("Kid(s):");
-		roomReservationPanel.add(lblKids, "1, 2, left, top");
+		roomReservationPanel.add(lblKids, "1, 4, left, top");
 		
 		kidSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
-		roomReservationPanel.add(kidSpinner, "2, 2, fill, top");
+		roomReservationPanel.add(kidSpinner, "2, 4, fill, top");
 		
 		JPanel roomButtonPanel = new JPanel();
-		roomPanel.add(roomButtonPanel, "2, 5, right, bottom");
+		roomPanel.add(roomButtonPanel, "2, 7, right, bottom");
 		roomButtonPanel.setLayout(new FormLayout(new ColumnSpec[] {
 				ColumnSpec.decode("185px"),
 				ColumnSpec.decode("75px"),
@@ -217,97 +235,6 @@ public class ReservationEditor extends JDialog implements Editor<Reservation>{
 		btnRemoveRoomReservation = new JButton("Remove");
 		btnRemoveRoomReservation.setEnabled(false);
 		roomButtonPanel.add(btnRemoveRoomReservation, "3, 2, right, top");
-		
-		JPanel stayPanel = new JPanel();
-		stayPanel.setBorder(new TitledBorder(null, "Stay", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_editor.add(stayPanel);
-		stayPanel.setLayout(new FormLayout(new ColumnSpec[] {
-				ColumnSpec.decode("15px"),
-				ColumnSpec.decode("150px"),
-				ColumnSpec.decode("195px"),},
-			new RowSpec[] {
-				RowSpec.decode("12dlu"),
-				RowSpec.decode("30px"),
-				RowSpec.decode("30px"),}));
-		
-		JLabel lblArrival = new JLabel("Arrival:");
-		stayPanel.add(lblArrival, "2, 2, left, top");
-		
-		Calendar cal = Calendar.getInstance();
-		Date now = new Date();
-		cal.setTime(now);
-		arrivalDateField = new JDateChooser(cal.getTime());
-		stayPanel.add(arrivalDateField, "3, 2, fill, top");
-		
-		JLabel lblDeparture = new JLabel("Departure:");
-		stayPanel.add(lblDeparture, "2, 3, left, top");
-		
-		cal.add(Calendar.DAY_OF_YEAR, 1);
-		departureDateField = new JDateChooser(cal.getTime());
-		stayPanel.add(departureDateField, "3, 3, fill, top");
-		
-		JPanel billPanel = new JPanel();
-		billPanel.setBorder(new TitledBorder(null, "Bill", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_editor.add(billPanel);
-		billPanel.setLayout(new FormLayout(new ColumnSpec[] {
-				ColumnSpec.decode("10dlu"),
-				ColumnSpec.decode("150px"),
-				ColumnSpec.decode("195px"),},
-			new RowSpec[] {
-				RowSpec.decode("12dlu"),
-				RowSpec.decode("30px"),
-				RowSpec.decode("30px"),}));
-		
-		JLabel lblPrice = new JLabel("Price:");
-		billPanel.add(lblPrice, "2, 2, left, top");
-		
-		priceSpinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 100000.0, 0.5));
-		billPanel.add(priceSpinner, "3, 2, fill, top");
-		
-		JLabel lblDiscount = new JLabel("Discount:");
-		billPanel.add(lblDiscount, "2, 3, left, top");
-		
-		discountSpinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 100000.0, 0.5));
-		billPanel.add(discountSpinner, "3, 3, fill, top");
-		
-		JPanel panel = new JPanel();
-		panel.setBorder(new EmptyBorder(0, 0, 0, 0));
-		getContentPane().add(panel, BorderLayout.SOUTH);
-		
-		JButton btnSave = new JButton("Save");
-		btnSave.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (ValidationHandler.validate(ReservationEditor.this)) {
-					ReservationEditor.this.accepted = true;
-					ReservationEditor.this.setVisible(false);
-				}
-			}
-		});
-		panel.add(btnSave);
-		
-		JButton btnCancel = new JButton("Cancel");
-		btnCancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				ReservationEditor.this.accepted = false;
-				ReservationEditor.this.setVisible(false);
-			}
-		});
-		panel.add(btnCancel);
-		
-		DAO dao = DAO.getInstance();
-		try {
-			customers = dao.getAll(Customer.class);
-			for (Customer c : customers){
-				customerBox.addItem(c);
-			}
-			rooms = dao.getAll(Room.class);
-			for (Room r : rooms){
-				roomBox.addItem(r);
-			}
-		} catch (DAOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		btnAddRoomReservation.addActionListener(new ActionListener() {
 			@Override
@@ -347,6 +274,101 @@ public class ReservationEditor extends JDialog implements Editor<Reservation>{
 				}
 			}
 		});
+		
+		JPanel panel_editor = new JPanel();
+		getContentPane().add(panel_editor);
+		panel_editor.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel stayPanel = new JPanel();
+		stayPanel.setBorder(new TitledBorder(null, "Stay", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_editor.add(stayPanel);
+		stayPanel.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("15px"),
+				ColumnSpec.decode("150px"),
+				ColumnSpec.decode("195px"),},
+			new RowSpec[] {
+				RowSpec.decode("12dlu"),
+				RowSpec.decode("30px"),
+				RowSpec.decode("30px"),}));
+		
+		JLabel lblArrival = new JLabel("Arrival:");
+		stayPanel.add(lblArrival, "2, 2, left, top");
+		arrivalDateField = new JDateChooser(cal.getTime());
+		stayPanel.add(arrivalDateField, "3, 2, fill, top");
+		
+		JLabel lblDeparture = new JLabel("Departure:");
+		stayPanel.add(lblDeparture, "2, 3, left, top");
+		departureDateField = new JDateChooser(cal.getTime());
+		stayPanel.add(departureDateField, "3, 3, fill, top");
+		
+		JPanel panel_5 = new JPanel();
+		getContentPane().add(panel_5);
+		
+		JPanel billPanel = new JPanel();
+		panel_5.add(billPanel);
+		billPanel.setBorder(new TitledBorder(null, "Bill", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		billPanel.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("15px"),
+				ColumnSpec.decode("150px"),
+				ColumnSpec.decode("195px"),},
+			new RowSpec[] {
+				RowSpec.decode("12dlu"),
+				RowSpec.decode("30px"),
+				RowSpec.decode("50px"),}));
+		
+		JLabel lblPrice = new JLabel("Price:");
+		billPanel.add(lblPrice, "2, 2, left, top");
+		
+		priceSpinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 100000.0, 0.5));
+		billPanel.add(priceSpinner, "3, 2, fill, top");
+		
+		JLabel lblDiscount = new JLabel("Discount:");
+		billPanel.add(lblDiscount, "2, 3, left, top");
+		
+		discountSpinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 100000.0, 0.5));
+		billPanel.add(discountSpinner, "3, 3, fill, top");
+		
+		JPanel panel_1 = new JPanel();
+		getContentPane().add(panel_1);
+		
+		JPanel panel = new JPanel();
+		panel_1.add(panel);
+		panel.setBorder(new EmptyBorder(0, 0, 0, 0));
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (ValidationHandler.validate(ReservationEditor.this)) {
+					ReservationEditor.this.accepted = true;
+					ReservationEditor.this.setVisible(false);
+				}
+			}
+		});
+		panel.add(btnSave);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ReservationEditor.this.accepted = false;
+				ReservationEditor.this.setVisible(false);
+			}
+		});
+		panel.add(btnCancel);
+		
+		DAO dao = DAO.getInstance();
+		try {
+			customers = dao.getAll(Customer.class);
+			for (Customer c : customers){
+				customerBox.addItem(c);
+			}
+			rooms = dao.getAll(Room.class);
+			for (Room r : rooms){
+				roomBox.addItem(r);
+			}
+		} catch (DAOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
